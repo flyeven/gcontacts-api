da8c17c12d196d724812e9e71969cebbb7448a92
tyler46 <mail.doctor46@gmail.com>
2015-01-31 11:53:53 +0200

    Extended version of Document.js

--- src/Document.js
+++ src/Document.js
@@ -1,9 +1,20 @@
 var xmlbuilder = require('xmlbuilder');
 var _ = require('lodash');
 
+
 function Document () {
-  this._root = xmlbuilder.create('root');
+
+  this._root = xmlbuilder.create('atom:entry');
+
+  this._root.att('xmlns:atom', 'http://www.w3.org/2005/Atom');
+  this._root.att('xmlns:gd', 'http://schemas/google.com/g/2005');
+
+  this._root.ele('atom:category', {
+    scheme: 'http://schemas.google.com/g/2005#kind',
+    term: 'http://schemas.google.com/contact/2008#contact'
+  });
 }
+
 
 Document.prototype.email = function (obj) {
   var ele;
@@ -24,9 +35,99 @@ Document.prototype.email = function (obj) {
   return ele;
 };
 
+
+Document.prototype.name = function (obj) {
+  var ele;
+
+  ele = this._root.ele('gd:name');
+
+  if (obj.givenName) ele.ele('gd:givenName', obj.givenName);
+  if (obj.familyName) ele.ele('gd:familyName', obj.familyName);
+  if (obj.fullName) ele.ele('gd:fullName', obj.fullName);
+
+  return ele;
+};
+
+
+Document.prototype.phone = function (obj) {
+  var ele;
+
+  obj = _.defaults(obj, {
+    primary: false,
+    rel: 'home'
+  });
+
+  ele = this._root.ele('gd:phoneNumber', {
+    rel: 'http://schemas.google.com/g/2005#' + obj.rel
+  }, obj.phonenNumber);
+
+  if (obj.primary) ele.att('primary', 'true');
+
+  return ele;
+};
+
+
+Document.prototype.im = function (obj) {
+  var ele;
+
+  obj = _.defaults(obj, {
+    primary: false,
+    protocol: 'GOOGLE_TALK',
+    rel: 'home'
+  });
+
+  ele = this._root.ele('gd:im', {
+    address: obj.address,
+    protocol: 'http://schemas.google.com/g/2005#' + obj.protocol,
+    rel: 'http://schemas.google.com/g/2005#' + obj.rel
+  });
+
+  if (obj.primary) ele.att('primary', 'true');
+
+  return ele;
+};
+
+
+Document.prototype.address = function (obj) {
+  var ele;
+
+  obj = _.defaults(obj, {
+    primary: false,
+    rel: 'home'
+  });
+
+  ele = this._root.ele('gd:structuredPostalAddress', {
+    rel: 'http://schemas.google.com/g/2005#' + obj.rel
+  });
+
+  if (obj.primary) ele.att('primary', 'true');
+
+  // Don't loop over element attributes.
+  _(obj)
+    .omit(['primary', 'rel'])
+    .forOwn(function (value, key) {
+      console.log(key);
+      ele.ele('gd:'+ key, value);
+    });
+
+  return ele;
+};
+
+
+Document.prototype.notes = function (obj) {
+  var ele;
+
+  ele = this._root.ele('gd:content', {
+    type: 'text'}, obj.text);
+
+  return ele;
+};
+
+
 Document.prototype.toString = function () {
   return this._root.end();
 };
+
 
 Document.fromJSON = function (obj) {
   var doc = new Document();
@@ -39,4 +140,5 @@ Document.fromJSON = function (obj) {
   return doc.toString();
 };
 
-console.log(Document.fromJSON({email: {address: 123}}));
+
+console.log(Document.fromJSON({email: {address: 123}, name: {fullName: 'Tom Bom'}, notes: {text: 'wtgsdg'}, im: {address: 'foo@mail.com'}, address: {city: 'Athens', street: 'LA Ave', country: 'Greece'}}));
